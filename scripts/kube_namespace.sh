b5224d164c788a6d02cbd1bc31334063650144d8
Corey Taylor <corey.taylor@ibotta.com>
2018-12-17 15:10:10 -0700

    Fixing kubectl config view changes in 1.13

--- scripts/kube_namespace.sh
+++ scripts/kube_namespace.sh
@@ -2,7 +2,7 @@
 
 if [[ -n "$(command -v kubectl)" && -n "$(command -v jq)"  ]]; then
     kubecluster=$(kubectl config current-context 2>/dev/null)
-    kubens=$(kubectl config view ${LP_K8S} --output json | jq ".contexts[] | select(.name==\"${kubecluster}\") | .context.namespace" | tr -d '"' || '')
+    kubens=$(kubectl config view --output json | jq ".contexts[] | select(.name==\"${kubecluster}\") | .context.namespace" | tr -d '"' || '')
 else
     kubens=
 fi
